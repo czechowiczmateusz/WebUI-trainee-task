bf3d7fccdcf38666415050046fb5f51d006ece0e
czechowiczmateusz <matic1.czechowicz@gmail.com>
2018-05-10 15:01:39 +0200

    2.0

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var sorting = function(number) {
-    var entry = [];
-    var numbers = Math.sqrt(number);
-    var output = [];
+let sorting = (number) => {
+    let entry = [];
+    let numbers = Math.sqrt(number);
+    let output = [];
 
-    for (var i = 0; i < number; i++) {
+    for (let i = 0; i < number; i++) {
         entry.push(true);
     }
 
-    for (var i = 2; i <= numbers; i++) {
+    for (let i = 2; i <= numbers; i++) {
         if (entry[i]) {
-            for (var j = i * i; j < number; j += i) {
+            for (let j = i * i; j < number; j += i) {
                 entry[j] = false;
             }
         }
     }
 
-    for (var i = 2; i < number; i++) {
+    for (let i = 2; i < number; i++) {
         if(entry[i]) {
             output.push(i);
         }
